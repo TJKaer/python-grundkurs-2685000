03b3461813dd3b21cf333afeb1cb0e2ffed0c944
TJKaer <t.lieb@gmx.de>
2026-02-05 12:16:04 +0000

    update

--- beispiel.py
+++ beispiel.py
@@ -20,7 +20,7 @@ class hund(Tier):
 class katze(Tier):
     def __init__(self,name: str, alter: str):
         super().__init__(name, alter)
-        print(f" mit {name} wurde eine neue Katze erstellt")
+        print(f" mit {self.name} wurde eine neue Katze erstellt")
 
     def macht_geräusche(self):
         print(f"{self.name} miaut")
@@ -31,3 +31,20 @@ whiskas = katze("whiskas", "4")
 print(hund1)
 hund1.macht_geräusche()
 whiskas.macht_geräusche()
+
+# Aufgabe:
+# Erstellen Sie eine weitere spezifische Klasse 'Vogel', die von der allgemeinen Klasse 'Tier' erbt.
+# - Überschreiben Sie die Methode 'mache_geraeusch', sodass der Vogel ein typisches Geräusch wie "zwitschern" macht.
+# - Erstellen Sie eine Instanz der Klasse 'Vogel' und rufen Sie die Methoden 'beschreibung' und 'mache_geraeusch' auf.
+
+class vogel(Tier):
+    def __init__(self,name: str, alter: str):
+        super().__init__(name, alter)
+        print(f" mit {self.name} wurde ein neuer Vogel erstellt")
+
+    def macht_geräusche(self):
+        print(f"{self.name} zwitschert")
+
+tweety = vogel("tweety","1")
+tweety.macht_geräusche()
+tweety.beschreibung()
